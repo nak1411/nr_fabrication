72c3fdddc4c339c1fbcad483bb7e10b28ffd2fd7
Justin <nak1411@gmail.com>
2018-11-21 18:49:32 -0800

    Route footer

---
+++ src/vue/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+<div id="footer">
+     <footer class="site__footer">
+        <div class="site__footersections">
+            <a class="site__footerspan" href="#">About Us</a>
+            <a class="site__footerspan" href="#">Contact Us</a>
+        </div>
+        <div class="site__trade">
+            <h3 class="site__trade">© 2018 NR Fabrication</h3>
+        </div>
+    </footer>
+    </div>
+</template>
