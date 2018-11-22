c7dca81b3f4fa4740a06dfb20a5122b2c980a747
Justin <nak1411@gmail.com>
2018-11-21 20:53:09 -0800

    Add content to about page

--- src/vue/components/About.vue
+++ src/vue/components/About.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="about">
     <div class="site__content">
-      <section class="site__services site__grid--single">
+      <section class="site__services site__grid--double">
         <div class="site_cell">
-          <h2 class="site__title--small">About Us</h2>
+          <h2 class="site__title">About NR Fabrication</h2>
           <p class="site__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatibus. Autem ab reprehenderit laudantium, nobis asperiores labore quidem animi similique ducimus quia doloribus ipsam! Eius nobis animi enim rem ratione.</p>
-        </div>
+          <p class="site__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatibus. Autem ab reprehenderit laudantium, nobis asperiores labore quidem animi similique ducimus quia doloribus ipsam! Eius nobis animi enim rem ratione.</p>        </div>
       </section>
     </div>
   </div>
