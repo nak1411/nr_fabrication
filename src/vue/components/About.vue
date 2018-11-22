82f39624a152165e12353e01fa84004d1fa0eb62
Justin <nak1411@gmail.com>
2018-11-21 22:15:04 -0800

    Add additional content to about page

--- src/vue/components/About.vue
+++ src/vue/components/About.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="about">
     <div class="site__content">
+      <h2 class="site__title">About NR Fabrication</h2>
       <section class="site__services site__grid--double">
-        <div class="site_cell">
-          <h2 class="site__title">About NR Fabrication</h2>
+        <div class="site__about--container">
           <p class="site__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatibus. Autem ab reprehenderit laudantium, nobis asperiores labore quidem animi similique ducimus quia doloribus ipsam! Eius nobis animi enim rem ratione.</p>
-          <p class="site__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatibus. Autem ab reprehenderit laudantium, nobis asperiores labore quidem animi similique ducimus quia doloribus ipsam! Eius nobis animi enim rem ratione.</p>        </div>
+          <p class="site__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatibus. Autem ab reprehenderit laudantium, nobis asperiores labore quidem animi similique ducimus quia doloribus ipsam! Eius nobis animi enim rem ratione.</p>
+          <p class="site__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatibus. Autem ab reprehenderit laudantium, nobis asperiores labore quidem animi similique ducimus quia doloribus ipsam! Eius nobis animi enim rem ratione.</p>
+
+        </div>
+        <img class="site__aboutimg" src="/src/img/about.jpg" alt="about nr fabrication placeholder image">
       </section>
     </div>
   </div>
