d012be6c638dc881b1e90ee53afeab0d10fce0dc
Justin <nak1411@gmail.com>
2018-11-29 13:20:09 -0800

    Add images and optimizations to gallery page

--- src/vue/components/Gallery.vue
+++ src/vue/components/Gallery.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="gallery">
     <div class="site__content">
+      <h2 class="site__gallerytitle">Gallery</h2>
+      <div class="site__gallerybreak"></div>
+      <section class="site__services site__grid--multi">
+        <div class="site_cell">
+          <img class="site__galleryimg" src="/src/img/bravata_thumb-min.jpg" alt="stylized sign for Bravata">
+        </div>
+        <div class="site_cell">
+          <img class="site__galleryimg" src="/src/img/fireball_01_thumb-min.jpg" alt="fireball">
+        </div>
+        <div class="site_cell">
+          <img class="site__galleryimg" src="/src/img/stainless_01_thumb-min.jpg" alt="stainless tig weld">
+        </div>
+        <div class="site_cell">
+          <img class="site__galleryimg" src="/src/img/stair_01_thumb-min.jpg" alt="black steel staircase">
+        </div>
+        <div class="site_cell">
+          <img class="site__galleryimg" src="/src/img/stair_02_thumb-min.jpg" alt="front door stair railings">
+        </div>
+        <div class="site_cell">
+          <img class="site__galleryimg" src="/src/img/stair_03_thumb-min.jpg" alt="interior wood and metal stair railing">
+        </div>
+      </section>
 
     </div>
   </div>
