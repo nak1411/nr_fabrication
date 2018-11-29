69e6bce744c3ab33270f11e5de52070c14011ec3
Justin <nak1411@gmail.com>
2018-11-29 14:07:38 -0800

    Start contact page

--- src/vue/components/Contact.vue
+++ src/vue/components/Contact.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="contact">
     <div class="site__content">
+      <h2 class="site__title">Contact NR Fabrication</h2>
+      <section class="site__services site__grid--double">
+        <div class="site__about--container">
+          <p class="site__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </section>
     </div>
   </div>
 </template>
